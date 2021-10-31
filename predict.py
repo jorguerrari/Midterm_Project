7f0813dda20fedab4ee7505e986b488dd94414b1
jorguerrari <jorginaguerra@gmail.com>
2021-10-31 17:14:01 +0100

    adding docker

--- predict.py
+++ predict.py
@@ -1,10 +1,31 @@
+import pickle
+
 from flask import Flask
+from flask import request
+from flask import jsonify
 
-app = Flask('ping')
 
-@app.route('/ping', methods=['GET'])
-def ping():
-    return "PONG"
+model_file = 'model.bin'
+
+with open(model_file, 'rb') as f_in:
+    dv, model = pickle.load(f_in)
+
+app = Flask('employee')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    employee = request.get_json()
+
+    X = dv.transform([employee])
+    y_pred = model.predict_proba(X)[0, 1]
+    
+
+    result = {
+        'leave_probability': float(y_pred),
+    }
+
+    return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=9696)
